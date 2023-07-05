4365cabd05d59334bcbdd7668e8a572e0076fcb1
imanadetunji <theiman.adetunji@gmail.com>
2023-07-05 16:29:45 -0400

    Fixed controller unit test and started integration test

--- src/test/java/com/udacity/DogRestApi/DogControllerUnitTest.java
+++ src/test/java/com/udacity/DogRestApi/DogControllerUnitTest.java
@@ -8,17 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(DogController.class)
+@WithMockUser(username = "admin", password = "password", roles = "USER")
 public class DogControllerUnitTest {
     @Autowired
     private MockMvc mockMvc;
@@ -28,7 +30,7 @@ public class DogControllerUnitTest {
 
     @Test
     public void getAllDogs() throws Exception {
-        mockMvc.perform(get("/dogs/"))
+        mockMvc.perform(get("/dogs"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(content().json("[]"));
@@ -44,5 +46,13 @@ public class DogControllerUnitTest {
                 .andExpect(content().json("[]"));
 
         verify(dogService, times(1)).retrieveDogBreed();
+    }
+
+    @Test
+    public void getDogBreedById() throws Exception {
+        mockMvc.perform(get("/1/breed"))
+                .andExpect(status().isOk());
+
+        verify(dogService, times(1)).retrieveDogBreedById(1L);
     }
 }
